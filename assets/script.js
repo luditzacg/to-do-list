6e97cf0daf2f48fb4184accd4905d44a7ae4e273
luditzacg <carolinaluditza@gmail.com>
2019-05-01 17:51:28 -0300

    Segunda parte

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,40 @@
+// esconder footer
+    // Footer
+    const footer = document.getElementById("btnContainer");
+    footer.style.display = "none";
+
 function btnAdd() {
 
     // Definindo a mãe
-    let lista = document.getElementById("userList");
+    const lista = document.getElementById("userList");
 
     // Criar o elemento (filho)
-    let item = document.createElement("li");
+    const item = document.createElement("li");
     
     // Definir o valor do input como texto
-    let valorInput = document.getElementById("userInput").value;
+    const valorInput = document.getElementById("userInput").value;
     
     // Criar texto 
-    let texto = document.createTextNode(valorInput);
+    const texto = document.createTextNode(valorInput);
+
+    
     
     // previne que o submit apague o input
     event.preventDefault();
 
+    // Mensagem de erro 
+    const error = document.getElementById("inputAlert")
+
+    
+   
     //validação do input do usuário
-    if(userInput.value.length == 0 || userInput.value.replace(/\s+/g, '').length == 0){
-        alert("Você precisa digitar uma tarefa!");
-        return;
+        if(userInput.value.length == 0 || userInput.value.replace(/\s+/g, "").length == 0){
+            error.innerHTML = 'Erro! Digite um texto válido.';
+            
+            return;
+    } else if (userInput.value.length != 0 || userInput.value.replace(/\s+/g, "").length != 0) {
+        error.style.display = "none";
+        footer.style.display = "flex";
     };
 
       // Anexar texto ao elemento
@@ -26,4 +42,41 @@ function btnAdd() {
     
       // Anexar o elemento filho (novo item) à mãe (ul)
       lista.appendChild(item)
-}›
+
+
+    // Cria botão para apagar individualmente
+    const eraser = document.getElementsByTagName("li");
+    let i;
+    for (i = 0; i < eraser.length; i++) {
+        const span = document.createElement("span");
+        const txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        eraser[i].appendChild(span);
+    }
+
+    // Riscar itens feitos
+    item.addEventListener('click', function () {
+        item.className = "checked"
+    });
+
+    /* Marcar todas como concluídas
+    Fica tudo em branco ao invés de puxar a classe
+        const selectAll = document.getElementById("checkAll");
+        const listSelect = document.getElementById("listSelect")
+
+        selectAll.addEventListener("click", function() {
+        listSelect.className = "checked"
+        }); */
+
+    //Limpar lista
+    //Depois que limpa, não add mais
+    const eraseAll = document.getElementById("eraseAll");
+    const listSelect = document.getElementById("listSelect")
+
+    eraseAll.addEventListener("click", function() {
+    listSelect.remove();
+    
+    });
+    
+}
